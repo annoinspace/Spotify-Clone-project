1f494c774a37771631d34a70d03d03d2dc61692d
Asdren JERLIU <114486010+asdreen@users.noreply.github.com>
2022-11-14 18:14:58 +0100

    100% done

--- log.js
+++ log.js
@@ -3,20 +3,18 @@ let attempt = 3;
 
 // Below function Executes on click of login button.
 function validate() {
-  var username = document.getElementById("login-username").value;
-  var password = document.getElementById("login-password").value;
-  if (username == "Formget" && password == "formget#123") {
+  let username = document.getElementById("login-username").value;
+  let password = document.getElementById("login-password").value;
+  if (username == "spotify" && password == "spotify2022") {
     alert("Login successfully");
-    window.location = "success.html"; // Redirecting to other page.
+    location.href = "https://www.spotify.com"; // Redirecting to other page.
     return false;
   } else {
     attempt--; // Decrementing by one.
     alert("You have left " + attempt + " attempt;");
     // Disabling fields after 3 attempts.
     if (attempt == 0) {
-      document.getElementById("login-username").disabled = true;
-      document.getElementById("login-password").disabled = true;
-      document.getElementById("loginCont").disabled = true;
+      alert("Your Password or Email is incorrect");
       return false;
     }
   }
